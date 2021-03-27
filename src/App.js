f02a1f1528f917d45a7db460a7e26d79544e9634
sadhika7 <swatiadhikari1997@gmail.com>
2021-03-27 11:29:31 -0500

    Added a Toggle Button on the form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AddTask from './components/AddTask';
 import {useState} from 'react';
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -43,8 +44,9 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-      <Header/>
-      <AddTask onAdd={addTask}/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)}
+      showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}
       onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,7 @@
 import PropTypes from 'prop-types'
 
-const Button = ({color, text}) => {
+const Button = ({color, text, onClick}) => {
 
-    const onClick = () => {
-        console.log('click')
-    }
     return (
         <button onClick={onClick}
         style={{backgroundColor: color}} 
